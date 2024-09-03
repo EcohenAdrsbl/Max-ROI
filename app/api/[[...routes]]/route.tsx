e477686562fbab2efe02550fa5c97722c1b6ac63
Eliyahu Cohen <eliyahucohen@Eliyahus-MacBook-Air.local>
2024-09-03 11:32:09 -0400

    first commit

---
+++ app/api/[[...routes]]/route.tsx
@@ -0,0 +1,41 @@
+/** @jsxImportSource frog/jsx */
+
+import { Button, Frog, TextInput } from 'frog'
+import { devtools } from 'frog/dev'
+// import { neynar } from 'frog/hubs'
+import { handle } from 'frog/next'
+import { serveStatic } from 'frog/serve-static'
+
+const app = new Frog({
+  basePath: '/api',
+  // hub: neynar({ apiKey: process.env.NEYMAR_API_KEY }),
+  browserLocation: '/',
+  imageOptions: {
+    fonts: [
+      {
+        name: 'JetBrains Mono',
+        source: 'google',
+      },
+    ],
+  },
+  title: 'Maxmize ROI',
+})
+
+// Uncomment to use Edge Runtime
+// export const runtime = 'edge'
+
+app.frame('/', (c) => {
+  const { buttonValue, inputText, status } = c
+  const fruit = inputText || buttonValue
+  return c.res({
+    image: 'https://i.ibb.co/JkYtsS3/Max-ROI.jpg',
+    intents: [
+      <Button.Link href = 'https://www.addressable.io/blog/5-proven-steps-to-skyrocket-your-roi-with-crypto-ads'>Keep Optimizing!</Button.Link>
+    ],
+  })
+})
+
+devtools(app, { serveStatic })
+
+export const GET = handle(app)
+export const POST = handle(app)
